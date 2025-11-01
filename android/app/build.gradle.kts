28551174d5ef5017105aaa8deced925b2d97c1cd
Amit Visrolia <amit@visrolia.net>
2025-11-01 13:14:15 +0100

    Added app name and ID for android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.french_verbs"
+        applicationId = "net.visrolia.french_verbs"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
